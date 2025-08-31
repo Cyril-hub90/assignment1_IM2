e022d94b0b17cd63bc635a21eda79b8fce4df0d1
Cyril-hub05 <22ur0863@psu.edu.ph>
2025-08-31 12:13:45 +0800

    v1

---
+++ LE_ONGJIANGCO/Models/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class EmployeeModel
+{
+    public int EmpId { get; set; }
+
+    [Required(ErrorMessage = "Full Name is required.")]
+    [MinLength(8, ErrorMessage = "Full Name must be at least 8 characters.")]
+    public string EmpFullname { get; set; }
+
+    [Required(ErrorMessage = "Address is required.")]
+    [MaxLength(30, ErrorMessage = "Address cannot exceed 30 characters.")]
+    public string EmpAddr { get; set; }
+
+    [Required(ErrorMessage = "Contact Number is required.")]
+    [MinLength(11, ErrorMessage = "Contact Number cannot exceed 11 characters.")]
+    public string EmpContact { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    [DataType(DataType.EmailAddress)]
+    public string EmpEmail { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
+    public string EmpPwd { get; set; }
+}
