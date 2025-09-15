e07110f6e8d92152332861a30fc226a0005f95ac
Cyril-hub05 <22ur0863@psu.edu.ph>
2025-09-15 18:21:50 +0800

    SW1

---
+++ source/repos/LE_ONGJIANGCO/LE_ONGJIANGCO/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class LoginModel
+{
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Provide valid email address.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
---
+++ source/repos/SW1/SW1_prob1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1
+{
+    class Program
+    {
+        
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            for (int i = 3; i * i <= number; i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+
+            
+            string[] parts = input.Split(' ');
+
+            int count = 1;
+            foreach (string part in parts)
+            {
+                if (int.TryParse(part, out int num))
+                {
+                    if (IsPrime(num))
+                        Console.WriteLine($"{count}.  {num}\tPrime");
+                    else
+                        Console.WriteLine($"{count}.  {num}\tComposite");
+                    count++;
+                }
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
