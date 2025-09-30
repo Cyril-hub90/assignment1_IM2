f03d117feebf4f413cbc53c5bb73131bebd8a3ee
Cyril-hub05 <22ur0863@psu.edu.ph>
2025-10-01 01:23:34 +0800

    Remove all files

--- source/repos/assignment1_IM2/HW3/assignment1_IM2/LE_ONGJIANGCO/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LE_ONGJIANGCO.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
