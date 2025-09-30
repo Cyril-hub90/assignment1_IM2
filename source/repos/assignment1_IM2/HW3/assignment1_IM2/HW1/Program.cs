0a5747342e228c3de417256eec5967b235c570a5
Cyril-hub05 <22ur0863@psu.edu.ph>
2025-10-01 00:33:04 +0800

    HW3

---
+++ source/repos/assignment1_IM2/HW3/assignment1_IM2/HW1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("\nEnter string: ");
+                string input = Console.ReadLine();
+
+                if (input == "*") 
+                    break;
+
+                string lowerInput = input.ToLower();
+                string reversed = "";
+
+                
+                for (int i = lowerInput.Length - 1; i >= 0; i--)
+                {
+                    reversed += lowerInput[i];
+                }
+
+                
+                string result = (lowerInput == reversed) ? "Yes" : "No";
+
+             
+                Console.WriteLine("\nOriginal String\t\tReversed String\t\tPalindrome?");
+                Console.WriteLine("  " + input + "\t\t   " + reversed + "\t\t    " + result);
+            }
+
+            Console.WriteLine("\n//End execution");
+        }
+    }
+}
