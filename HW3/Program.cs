467fd0618d8006c482be8ec909d4229f16630996
Cyril-hub05 <22ur0863@psu.edu.ph>
2025-10-01 01:30:34 +0800

    Added HW3 project

---
+++ HW3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            
+            Dictionary<char, int> frequencies = GetCharacterFrequency(input);
+
+            
+            Console.WriteLine("\nCharacter Frequencies:");
+            foreach (var kvp in frequencies)
+            {
+                Console.WriteLine($"{kvp.Key} = {kvp.Value}");
+            }
+
+            
+            Console.ReadKey();
+        }
+
+        
+        static Dictionary<char, int> GetCharacterFrequency(string text)
+        {
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c)) 
+                {
+                    if (freq.ContainsKey(c))
+                        freq[c]++;
+                    else
+                        freq[c] = 1;
+                }
+            }
+
+            return freq;
+        }
+    }
+}
